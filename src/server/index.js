2f9542f16acc487a0be05c49b167f2420efbda48
Fred Block <info@fredblock.com>
2020-05-13 16:51:36 -0500

    Add AYLIENTextAPI to server>index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,50 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+/*jshint esversion: 6 */
 
-const app = express()
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use(express.static('dist'))
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-console.log(__dirname)
+var AYLIENTextAPI = require('aylien_textapi');
+// set aylien API credentias
+var textapi = new AYLIENTextAPI({
+  application_id: process.env.APP_ID,
+  application_key: process.env.API_KEY
+});
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+const app = express();
+app.use(express.static('dist'));
+
+console.log(__dirname);
+
+
+
+// ROUTES ////////////////////////////////
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+  console.log('Example app listening on port 8081!');
+});
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'));
+});
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+    res.send(mockAPIResponse);
+});
+
+
+
+// AYLIEN API /////////////////////////////
+
+textapi.sentiment({
+  'text': 'John is a very good football player!'
+}, function(error, response) {
+  if (error === null) {
+    console.log(response);
+  }
+});
