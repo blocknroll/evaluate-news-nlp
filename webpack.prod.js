601d62bd71a0d706f22fde20068f50e49b89d13b
Fred Block <info@fredblock.com>
2020-05-13 12:37:59 -0500

    Update webpack.prod.js with OptimizeCSSAssetsPlugin and TerserPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -22,6 +24,9 @@ module.exports = {
                 use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
     plugins: [
         new HtmlWebPackPlugin({
